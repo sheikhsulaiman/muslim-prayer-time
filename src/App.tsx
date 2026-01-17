f8eab8d2edf3683ae0de22c90c203369dd522660
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2026-01-17 07:17:42 +0600

    import Prayer time component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
+import PrayerTimes from "@/components/PrayerTimes";
 
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <PrayerTimes />;
 }
 
 export default App;
